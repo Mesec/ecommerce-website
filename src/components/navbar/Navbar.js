9c19940ed38d84917eedf3b7983561e84531f44d
Mesec <rakonjacstefa@gmail.com>
2023-08-24 18:37:38 +0200

    Navbar mobile alignment

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -34,44 +34,45 @@ function ResponsiveAppBar() {
     <AppBar color='primary' position="static">
       <Container maxWidth="xl">
         <Toolbar disableGutters>
-          <Box display='flex' paddingLeft='160px'>
-            <Box sx={ { display: { md: 'none' } } }>
+          <Box width='100%' justifyContent='space-between' display='flex'>
+            <Box display='flex' md={ { paddingLeft: '160px' } }>
+              <Box sx={ { display: { md: 'none' } } }>
+                <IconButton
+                  size="large"
+                  aria-label="account of current user"
+                  aria-controls="menu-appbar"
+                  aria-haspopup="true"
+                  onClick={ handleOpenNavMenu }
+                  color="secondary"
+                >
+                  <MenuIcon />
+                </IconButton>
+                <Menu
+                  id="menu-appbar"
+                  anchorEl={ anchorElNav }
+                  anchorOrigin={ {
+                    vertical: 'bottom',
+                    horizontal: 'left',
+                  } }
+                  keepMounted
+                  transformOrigin={ {
+                    vertical: 'top',
+                    horizontal: 'left',
+                  } }
+                  open={ Boolean(anchorElNav) }
+                  onClose={ handleCloseNavMenu }
+                  sx={ {
+                    display: { xs: 'block', md: 'none' },
+                  } }
+                >
+                  { pages.map((page) => (
+                    <MenuItem key={ page } onClick={ handleCloseNavMenu }>
+                      <Typography textAlign="center">{ page }</Typography>
+                    </MenuItem>
+                  )) }
+                </Menu>
+              </Box>
               <IconButton
-                size="large"
-                aria-label="account of current user"
-                aria-controls="menu-appbar"
-                aria-haspopup="true"
-                onClick={ handleOpenNavMenu }
-                color="secondary"
-              >
-                <MenuIcon />
-              </IconButton>
-              <Menu
-                id="menu-appbar"
-                anchorEl={ anchorElNav }
-                anchorOrigin={ {
-                  vertical: 'bottom',
-                  horizontal: 'left',
-                } }
-                keepMounted
-                transformOrigin={ {
-                  vertical: 'top',
-                  horizontal: 'left',
-                } }
-                open={ Boolean(anchorElNav) }
-                onClose={ handleCloseNavMenu }
-                sx={ {
-                  display: { xs: 'block', md: 'none' },
-                } }
-              >
-                { pages.map((page) => (
-                  <MenuItem key={ page } onClick={ handleCloseNavMenu }>
-                    <Typography textAlign="center">{ page }</Typography>
-                  </MenuItem>
-                )) }
-              </Menu>
-            </Box>
-               <IconButton
                 size="small"
                 aria-label="account of current user"
                 aria-controls="menu-appbar"
@@ -79,27 +80,28 @@ function ResponsiveAppBar() {
                 onClick={ handleOpenNavMenu }
                 color="secondary"
               >
-              <img style={ { width: '143px', height: '25px' } } src={ Logo } alt='Audiophile'/>
+                <img style={ { width: '143px', height: '25px' } } src={ Logo } alt='Audiophile' />
               </IconButton>
-          </Box>
-          <Box 
-          sx={ {
-            flexGrow: 1,
-            gap: 2,
-            display: { xs: 'none', md: 'flex' },
-            justifyContent: 'center',
-            alignItems: 'center',
-            fontFamily: theme.typography.fontFamily
-          } }>
-            { pages.map((page) => (
-              <Link href="#" className='Link'>
-                { page }
-              </Link>
-            )) }
-          </Box>
-            <Box display='flex' paddingRight='160px'>
+            </Box>
+            <Box
+              sx={ {
+                flexGrow: 1,
+                gap: 2,
+                display: { xs: 'none', md: 'flex' },
+                justifyContent: 'center',
+                alignItems: 'center',
+                fontFamily: theme.typography.fontFamily
+              } }>
+              { pages.map((page) => (
+                <Link href="#" className='Link'>
+                  { page }
+                </Link>
+              )) }
+            </Box>
+            <Box display='flex' md={ { paddingRight: '160px' } }>
               <img src={ Cart } alt="Add to Cart" />
             </Box>
+          </Box>
         </Toolbar>
       </Container>
     </AppBar>
