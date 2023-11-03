d7c1d6db5f9f6533ab6c901ca247a2366bca3c1a
Mesec <rakonjacstefa@gmail.com>
2023-11-03 15:10:32 +0100

    Navbar refactored & Promoted component created

---
+++ src/components/Promoted/Promoted.js
@@ -0,0 +1,27 @@
+import { Box, Button, Typography } from '@mui/material'
+import React from 'react'
+import { Link } from 'react-router-dom'
+import './Promoted.css'
+
+export default function Promoted() {
+  return (
+    <Box className='Home-Header-Container'>
+      <Box className='Home-Header'>
+        <Typography variant='h7' color='secondary' fontSize='14px'>
+          NEW PRODUCT
+        </Typography>
+        <Typography variant='h4' color='secondary' fontSize='15px'>
+          XX99 Mark II Headphones
+        </Typography>
+        <Typography variant='body1' color='secondary'>
+          Experience natural, lifelike audio and exceptional build quality made for the passionate music enthusiast.
+        </Typography>
+        <Link to={ `/product/h-01` }>
+          <Button variant="contained">
+            NEW PRODUCT
+          </Button>
+        </Link>
+      </Box>
+    </Box>
+  )
+}
--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -16,6 +16,7 @@ import './Navbar.css';
 import { pages } from '../../app/constants';
 import NavigationItems from './NavigationItems/NavigationItems';
 import Button from '@mui/material/Button';
+import Promoted from '../Promoted/Promoted';
 
 function ResponsiveAppBar({ openCartModal }) {
   const [anchorElNav, setAnchorElNav] = React.useState(null)
@@ -32,65 +33,72 @@ function ResponsiveAppBar({ openCartModal }) {
   const borderPathNames = ['/', '/headphones', '/speakers', '/earphones'];
 
   return (
-    <AppBar color='primary' position="static" style={ { padding: '0 165px' }}>
-      <Container maxWidth="xxl" style={ { borderBottom: borderPathNames.includes(location.pathname) ? '1px solid rgba(255, 255, 255, 0.2)' : 'none' } }>
-        <Toolbar disableGutters>
-          <Box width='100%' justifyContent='space-between' display='flex'>
-            <Box display='flex'>
-              <Box sx={ { display: { md: 'none' } } }>
+    <Box width='100%'>
+      <AppBar color='primary' position="static" style={ { padding: '0 348px' } }>
+        <Container maxWidth="xxl" style={ { borderBottom: borderPathNames.includes(location.pathname) ? '1px solid rgba(255, 255, 255, 0.2)' : 'none', padding: '7px 0 7px 0' } }>
+          <Toolbar disableGutters>
+            <Box width='100%' justifyContent='space-between' display='flex'>
+              <Box display='flex'>
+                <Box sx={ { display: { md: 'none' } } }>
+                  <IconButton
+                    size="large"
+                    aria-label="account of current user"
+                    aria-controls="menu-appbar"
+                    aria-haspopup="true"
+                    onClick={ handleOpenNavMenu }
+                    color="secondary"
+                  >
+                    <MenuIcon />
+                  </IconButton>
+                  <Menu
+                    id="menu-appbar"
+                    anchorEl={ anchorElNav }
+                    anchorOrigin={ {
+                      vertical: 'bottom',
+                      horizontal: 'left',
+                    } }
+                    keepMounted
+                    transformOrigin={ {
+                      vertical: 'top',
+                      horizontal: 'left',
+                    } }
+                    open={ Boolean(anchorElNav) }
+                    onClose={ handleCloseNavMenu }
+                    sx={ {
+                      display: { xs: 'block', md: 'none' },
+                    } }
+                  >
+                    { pages.map((page) => (
+                      <MenuItem key={ page.url } onClick={ handleCloseNavMenu }>
+                        <Typography textAlign="center">{ page.name }</Typography>
+                      </MenuItem>
+                    )) }
+                  </Menu>
+                </Box>
                 <IconButton
-                  size="large"
+                  size="small"
                   aria-label="account of current user"
                   aria-controls="menu-appbar"
                   aria-haspopup="true"
-                  onClick={ handleOpenNavMenu }
                   color="secondary"
                 >
-                  <MenuIcon />
+                  <img style={ { width: '143px', height: '25px' } } src={ Logo } alt='Audiophile' />
                 </IconButton>
-                <Menu
-                  id="menu-appbar"
-                  anchorEl={ anchorElNav }
-                  anchorOrigin={ {
-                    vertical: 'bottom',
-                    horizontal: 'left',
-                  } }
-                  keepMounted
-                  transformOrigin={ {
-                    vertical: 'top',
-                    horizontal: 'left',
-                  } }
-                  open={ Boolean(anchorElNav) }
-                  onClose={ handleCloseNavMenu }
-                  sx={ {
-                    display: { xs: 'block', md: 'none' },
-                  } }
-                >
-                  { pages.map((page) => (
-                    <MenuItem key={ page.url } onClick={ handleCloseNavMenu }>
-                      <Typography textAlign="center">{ page.name }</Typography>
-                    </MenuItem>
-                  )) }
-                </Menu>
               </Box>
-              <IconButton
-                size="small"
-                aria-label="account of current user"
-                aria-controls="menu-appbar"
-                aria-haspopup="true"
-                color="secondary"
-              >
-                <img style={ { width: '143px', height: '25px' } } src={ Logo } alt='Audiophile' />
-              </IconButton>
-            </Box>
               <NavigationItems />
-            <Button onClick={ openCartModal }>
-              <img src={ Cart } alt="Add to Cart" />
-            </Button>
-          </Box>
-        </Toolbar>
-      </Container>
-    </AppBar>
+              <Button onClick={ openCartModal }>
+                <img src={ Cart } alt="Add to Cart" />
+              </Button>
+            </Box>
+          </Toolbar>
+        </Container>
+      </AppBar>
+      { location.pathname === '/' && <Promoted/>}
+      { (location.pathname === '/speakers' || location.pathname === '/headphones' || location.pathname === 'earphones')
+        && <Box className='Product-Header'>
+          <Typography variant='h3'>{ location.pathname } </Typography>
+        </Box>}
+    </Box>
   );
 }
 export default ResponsiveAppBar;
