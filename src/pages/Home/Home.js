63dbba0e6d1d47351b61ada8e430f1e077cae1b3
Mesec <rakonjacstefa@gmail.com>
2025-01-18 15:57:37 +0100

    featured items reponsive design

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -5,35 +5,16 @@ import './Home.css'
 import ProductNavigation from '../../components/ProductNavigation/ProductNavigation'
 import { Link } from 'react-router-dom'
 import FeaturedItemOne from 'components/FeaturedItems/FeaturedItemOne'
+import FeaturedItemTwo from 'components/FeaturedItems/FeaturedItemTwo'
+import FeaturedItemThree from 'components/FeaturedItems/FeaturedItemThree'
 
 export default function Home() {
   return (
     <Box className='Home-Main-Content'>
       <ProductNavigation />
       <FeaturedItemOne />
-      <Grid container className='Home-Main-Content'>
-        <Grid lg={ 10 } item className='Home-Second'>
-          <Box>
-            <Typography variant='h4'>
-              ZX7 SPEAKER
-            </Typography>
-            <Link to={ `/product/s-02` }>
-              <Button variant="outlined">See Product</Button>
-            </Link>
-          </Box>
-        </Grid>
-        <Grid lg={ 10 } item className='Home-Third'>
-          <Box className='Home-Third-Earphones'></Box>
-          <Box>
-            <Typography variant='h4'>
-              YX1 EARPHONES
-            </Typography>
-            <Link to={ `/product/e-01` }>
-              <Button variant="outlined">See Product</Button>
-            </Link>
-          </Box>
-        </Grid>
-      </Grid>
+      <FeaturedItemTwo/>
+      <FeaturedItemThree />
       </Box>
   )
 }
