e5a03b868eb2d992d4ef735155312237dc4de9cb
Mesec <rakonjacstef@gmail.com>
2025-02-20 00:01:24 +0100

    redirect scroll fixed

--- src/components/ScrollToTop/ScrollToTop.js
+++ src/components/ScrollToTop/ScrollToTop.js
@@ -1,15 +1,10 @@
 import { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
 
 const ScrollToTop = ({ children }) => {
-  console.log(children)
-  const pathname = window.location.pathname;
-  console.log(pathname)
+  const pathname = useLocation().pathname;
   useEffect(() => {
-    console.log('TEST')
-    document.documentElement.scrollTop = 0;
-    document.body.scrollTop = 0;
     window.scrollTo(0, 0);
-    console.log(window)
   }, [pathname]);
 
   return (
