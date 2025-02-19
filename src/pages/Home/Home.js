e878e05ab8c2daaca2e33214b1850628c54a1cc2
Mesec <rakonjacstef@gmail.com>
2025-02-19 11:39:40 +0100

    commercial styled

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -14,8 +14,8 @@ export default function Home() {
         <FeaturedItemOne />
         <FeaturedItemTwo />
         <FeaturedItemThree />
-        <Grid container className='TopFooter'>
-          <Grid xl={ 6 } lg={ 6 } md={ 12 } item>
+        <Grid container className='Commercial'>
+          <Grid className='Commercial-Info' xl={ 6 } lg={ 6 } md={ 12 } item>
             <Typography variant='h4'>
               Bringing you the <Box component="span">best</Box> audio gear
             </Typography>
@@ -23,7 +23,7 @@ export default function Home() {
               Located at the heart of New York City, Audiophile is the premier store for high end headphones, earphones, speakers, and audio accessories. We have a large showroom and luxury demonstration rooms available for you to browse and experience a wide range of our products. Stop by our store to meet some of the fantastic people who make Audiophile the best place to buy your portable audio equipment.
             </Typography>
           </Grid>
-          <Grid xl={ 6 } lg={ 6 } md={ 12 } item className='Footer-Image'></Grid>
+          <Grid xl={ 6 } lg={ 6 } md={ 12 } item className='Commercial-Image'></Grid>
         </Grid>
       </Box>
     </Box>
