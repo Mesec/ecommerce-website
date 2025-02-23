8fae97164288c905c20d098db5a607f22a29d0f1
Mesec <rakonjacstef@gmail.com>
2025-02-23 20:12:25 +0100

    Gallery responsiveness for mobile devices

--- src/components/Gallery/Gallery.js
+++ src/components/Gallery/Gallery.js
@@ -9,7 +9,7 @@ import { useMediaQuery } from "@mui/material";
 
 const Gallery = ({ images }) => {
   const mainImage = require(`../../assets/images/products${images[0]}`);
-  const isSmallScreen = useMediaQuery("(max-width: 900px)");
+  const isSmallScreen = useMediaQuery("(max-width: 600px)");
 
   const [selectedImage, setSelectedImage] = useState(mainImage);
   const [startIndex, setStartIndex] = useState(0);
@@ -34,9 +34,9 @@ const Gallery = ({ images }) => {
     <>
         <Box className="gallery-container">
           <Box className="thumbnails-container">
-            <Button disabled={ !canSlideUp } onClick={ slideUp } className="arrow-button">
-              <KeyboardArrowLeftIcon/>
-            </Button>
+            {/* <Button disabled={ !canSlideUp } onClick={ slideUp } className="arrow-button"> */}
+            <KeyboardArrowLeftIcon onClick={ slideUp } className="arrow-button" />
+            {/* </Button> */}
             <Box className="thumbnails">
               { images.slice(startIndex, startIndex + thumbnailsToShow).map((img, index) => {
                 const thumbnail = require(`../../assets/images/products${img}`);
@@ -52,9 +52,9 @@ const Gallery = ({ images }) => {
                 )
               }) }
             </Box>
-            <Button disabled={ !canSlideDown } onClick={ slideDown } className="arrow-button">
-              <KeyboardArrowRightIcon/>
-            </Button>
+            {/* <Button disabled={ !canSlideDown } onClick={ slideDown } className="arrow-button"> */}
+              <KeyboardArrowRightIcon disabled={ !canSlideDown } onClick={ slideDown } className="arrow-button" />
+            {/* </Button> */}
           </Box>
           <Box className='main-image-container'>
             <img src={ selectedImage } alt="Main" className="main-image" />
